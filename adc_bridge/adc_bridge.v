8f586a7d7f73ee1bd69a9e9bf4a3fef742337513
Harald Pretl <harald@pretl.eu>
2023-11-03 16:03:01 +0100

    Fixed logic flaw in adc_bridge to only write config once after reset, also make conv_finish signal selectable via cfg

--- adc_bridge/adc_bridge.v
+++ adc_bridge/adc_bridge.v
@@ -8,50 +8,64 @@
 
 module adc_bridge (
     // chip inputs
-    input wire              clk,        // clk for shift regs
-    input wire              rst_n,      // async. reset for regs
-    input wire              dat_i,      // serial in (ADC config, LSB first)
-    input wire              load,       // load strobe for shift regs
+    input wire              clk,            // clk for shift regs
+    input wire              rst_n,          // async. reset for regs
+    input wire              dat_i,          // serial in (ADC config, LSB first)
+    input wire              load,           // load strobe for shift regs
     // interface to ADC
-    input wire [15:0]       adc_res,    // ADC result input
-    output wire [15:0]      adc_cfg1,   // ADC config1 output
-    output wire [15:0]      adc_cfg2,   // ADC config2 output
+    input wire [15:0]       adc_res,        // ADC result input
+    input wire              adc_conv_finished,
+    input wire              adc_conv_finished_osr,
+    output wire [15:0]      adc_cfg1,       // ADC config1 output
+    output wire [15:0]      adc_cfg2,       // ADC config2 output
     // chip outputs
-    output wire             dat_o,      // serial out (ADC result, LSB first)
-    output wire             tie1,       // logic 1 aux. output
-    output wire             tie0        // logic 2 aux. output
+    output wire             dat_o,          // serial out (ADC result, LSB first)
+    output wire             conv_finish,    // flag that conversion is finished
+    output wire             tie1,           // logic 1 aux. output
+    output wire             tie0            // logic 2 aux. output
     );
 
     // define local register used for shifting and storing
-    reg [31:0] adc_cfg_store_r;
-    reg [31:0] adc_cfg_load_r;
-    reg [19:0] adc_res_r;
+    reg [32:0]              adc_cfg_store_r;
+    reg [32:0]              adc_cfg_load_r;
+    reg [19:0]              adc_res_r;
+    reg                     adc_cfg_written_r;
+    wire                    conv_finish_sel;
 
     // outputs
     assign dat_o = adc_res_r[0];
     assign adc_cfg1 = adc_cfg_store_r[15:0];
     assign adc_cfg2 = adc_cfg_store_r[31:16];
+    assign conv_finish_sel = adc_cfg_store_r[32];
     assign tie1=1'b1;
     assign tie0=1'b0;
+
+    assign conv_finish = (conv_finish_sel == 1'b0) ?
+                            adc_conv_finished_osr :
+                            adc_conv_finished; 
 
     // on clk, shift config in, and result out (LSB first)
     always @(posedge clk or negedge rst_n) begin
         if (rst_n == 1'b0) begin
             // reset
-            adc_cfg_store_r <= 32'd0;
-            adc_cfg_load_r <= 32'd0;
+            adc_cfg_written_r <= 1'b0;
+            adc_cfg_store_r <= 33'd0;
+            adc_cfg_load_r <= 33'd0;
             adc_res_r <= 20'd0;
         end else begin
             if (clk == 1'b1) begin
                 if (load == 1'b0) begin
                     // shift
                     adc_res_r <= {1'b0,adc_res_r[19:1]};
-                    adc_cfg_load_r <= {dat_i,adc_cfg_load_r[31:1]};
+                    adc_cfg_load_r <= {dat_i,adc_cfg_load_r[32:1]};
                 end else begin
                     // store
                     // add a bit of framing 10xxxx01 for easier detection of correct data
                     adc_res_r <= {2'b10,adc_res,2'b01};
-                    adc_cfg_store_r <= adc_cfg_load_r;
+                    if (adc_cfg_written_r == 1'b0) begin
+                        adc_cfg_written_r <= 1'b1;
+                        adc_cfg_store_r <= adc_cfg_load_r;
+                    end
                 end
             end
         end
--- adc_bridge/adc_bridge_tb.v
+++ adc_bridge/adc_bridge_tb.v
@@ -18,7 +18,11 @@ module adc_bridge_tb;
     wire TIE1;
     wire TIE0;
 
-    reg [31:0] ADC_CFG_IN = {16'h1234,16'h5678};
+    reg ADC_CONV_FINISHED = 1'b0;
+    reg ADC_CONV_FINISHED_OSR = 1'b0;
+    wire CONV_FINISH;
+
+    reg [32:0] ADC_CFG_IN = {1'b1,16'h1234,16'h5678};
     reg [19:0] ADC_RES_OUT;
 
     wire [15:0] ADC_RES_OUT_MIDDLE;
@@ -32,6 +36,9 @@ module adc_bridge_tb;
         .dat_i(DAT_I),
         .load(LOAD),
         .adc_res(ADC_RESULT),
+        .adc_conv_finished(ADC_CONV_FINISHED),
+        .adc_conv_finished_osr(ADC_CONV_FINISHED_OSR),
+        .conv_finish(CONV_FINISH),
         .adc_cfg1(ADC_CFG1),
         .adc_cfg2(ADC_CFG2),
         .dat_o(DAT_O),
@@ -44,27 +51,29 @@ module adc_bridge_tb;
         $dumpvars;
 
         // first, do a reset
-
         #1 RST_N = 1'b0;
         #1 RST_N = 1'b1;
 
-        // then load the config register serially
+        // pulse the conversion finished
+        #1 ADC_CONV_FINISHED = 1'b1;
+        #1 ADC_CONV_FINISHED = 1'b0;
+        #1 ADC_CONV_FINISHED_OSR = 1'b1;
+        #1 ADC_CONV_FINISHED_OSR = 1'b0;
 
-        for (i = 0; i < 32; i = i + 1) begin
+        // then load the config register serially
+        for (i = 0; i < 33; i = i + 1) begin
             DAT_I = ADC_CFG_IN[i];
             #1 CLK = 1'b1;
             #1 CLK = 1'b0;
         end 
 
         // do a load cycle (cfg in, adc result out)
-
            LOAD = 1'b1;
         #1 CLK = 1'b1;
         #1 CLK = 1'b0; 
            LOAD = 1'b0;
 
         // then shit the adc result out
-
         for (i = 0; i < 20; i = i + 1) begin
             ADC_RES_OUT[i] = DAT_O;
             #1 CLK = 1'b1;
@@ -72,11 +81,39 @@ module adc_bridge_tb;
         end
 
         // now check for correct result (mini assertion)
-
         if (ADC_CFG1 != 16'h5678) $display("[ERROR] CFG1 state wrong!");
         if (ADC_CFG2 != 16'h1234) $display("[ERROR] CFG2 state wrong!");
         if (ADC_RES_OUT[17:2] != 16'habcd) $display("[ERROR] RES state wrong!");
 
+        // pulse the conversion finished
+        #1 ADC_CONV_FINISHED = 1'b1;
+        #1 ADC_CONV_FINISHED = 1'b0;
+        #1 ADC_CONV_FINISHED_OSR = 1'b1;
+        #1 ADC_CONV_FINISHED_OSR = 1'b0;
+
+
+        // now get a 2nd ADC result, and check that config is not corrupted
+        ADC_RESULT = 16'hdcba;
+
+        // do a load cycle (cfg in, adc result out)
+           LOAD = 1'b1;
+        #1 CLK = 1'b1;
+        #1 CLK = 1'b0; 
+           LOAD = 1'b0;
+
+       // then shit the adc result out
+        for (i = 0; i < 20; i = i + 1) begin
+            ADC_RES_OUT[i] = DAT_O;
+            #1 CLK = 1'b1;
+            #1 CLK = 1'b0;
+        end
+
+        // now check for correct result (mini assertion)
+        if (ADC_CFG1 != 16'h5678) $display("[ERROR] CFG1 state wrong!");
+        if (ADC_CFG2 != 16'h1234) $display("[ERROR] CFG2 state wrong!");
+        if (ADC_RES_OUT[17:2] != 16'hdcba) $display("[ERROR] RES state wrong!"); 
+
+        // and done
         #5 $finish;
     end
 
