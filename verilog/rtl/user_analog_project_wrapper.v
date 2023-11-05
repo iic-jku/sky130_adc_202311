472ad6868477b46e8bd40029c4829e6689229179
Harald Pretl <harald@pretl.eu>
2023-11-05 12:16:01 +0100

    Try to fix consistency check

--- verilog/rtl/user_analog_project_wrapper.v
+++ verilog/rtl/user_analog_project_wrapper.v
@@ -123,23 +123,58 @@ module user_analog_project_wrapper (
 /* User project is instantiated  here   */
 /*--------------------------------------*/
 
-adc_wrapper adcwrap0 (
-   `ifdef USE_POWER_PINS
-        .VDD(vccd1),  // User area 1 1.8V power
-        .VSS(vssd1),  // User area 1 digital ground
+adc_wrapper mprj (
+    `ifdef USE_POWER_PINS
+        .vdda1(vdda1),  // User area 1 3.3V power
+        .vdda2(vdda2),  // User area 2 3.3V power
+        .vssa1(vssa1),  // User area 1 analog ground
+        .vssa2(vssa2),  // User area 2 analog ground
+        .vccd1(vccd1),  // User area 1 1.8V power
+        .vccd2(vccd2),  // User area 2 1.8V power
+        .vssd1(vssd1),  // User area 1 digital ground
+        .vssd2(vssd2),  // User area 2 digital ground
     `endif
 
-    .rst_n(io_in[11]),
-    .clk(io_in[13]),
-    .conv_start(io_in[12]),
-    .load(io_in[8]),
-    .dati(io_in[7]),
-    .conv_finish(io_out[5]),
-    .dato(io_out[6]),
-    .tie0(io_oeb[6:5]),
-    .tie1(io_oeb[13:7]),
-    .inp_ana(io_analog[3]),
-    .inn_ana(io_analog[2])
+    .wb_clk_i(wb_clk_i),
+    .wb_rst_i(wb_rst_i),
+
+    // MGMT SoC Wishbone Slave
+
+    .wbs_cyc_i(wbs_cyc_i),
+    .wbs_stb_i(wbs_stb_i),
+    .wbs_we_i(wbs_we_i),
+    .wbs_sel_i(wbs_sel_i),
+    .wbs_adr_i(wbs_adr_i),
+    .wbs_dat_i(wbs_dat_i),
+    .wbs_ack_o(wbs_ack_o),
+    .wbs_dat_o(wbs_dat_o),
+
+    // Logic Analyzer
+
+    .la_data_in(la_data_in),
+    .la_data_out(la_data_out),
+    .la_oenb (la_oenb),
+
+    // IO Pads
+    .io_in (io_in),
+    .io_in_3v3 (io_in_3v3),
+    .io_out(io_out),
+    .io_oeb(io_oeb),
+
+    // GPIO-analog
+    .gpio_analog(gpio_analog),
+    .gpio_noesd(gpio_noesd),
+
+    // Dedicated analog
+    .io_analog(io_analog),
+    .io_clamp_high(io_clamp_high),
+    .io_clamp_low(io_clamp_low),
+
+    // Clock
+    .user_clock2(user_clock2),
+
+    // IRQ
+    .irq(user_irq)
 );
 
 endmodule	// user_analog_project_wrapper
